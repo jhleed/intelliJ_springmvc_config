519bd48d58e1ae3b7d13cd81c5377d0b467894fd
JongHo Lee <jhleed@radnet.kr>
2017-03-10 09:54:43 +0900

    controller 및 helloj.jsp 추가

---
+++ src/main/java/james/controller/SampleController.java
@@ -0,0 +1,25 @@
+package james.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SampleController {
+
+    @RequestMapping("/view/{viewName}")
+    public String view(
+            @PathVariable("viewName") String viewName
+    ) {
+        return viewName;
+    }
+
+    @RequestMapping("/value/{value}")
+    @ResponseBody
+    public String value(
+            @PathVariable("value") String value
+    ){
+        return value;
+    }
+}
---
+++ src/main/webapp/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: JongHo
+  Date: 2017-03-10
+  Time: 오전 9:50
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>hello.jsp 페이지 입니다.</h1>;
+</body>
+</html>
